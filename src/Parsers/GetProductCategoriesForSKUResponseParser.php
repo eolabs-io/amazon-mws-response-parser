25d7bdc816d6336da5f0065ccf0c9c1e9f0bd0b6
tim hall <timhall@tims-mbp.lan>
2021-04-01 14:06:41 -0500

    fixed issue with ProductCategories not safely getting key value

--- src/Parsers/GetProductCategoriesForSKUResponseParser.php
+++ src/Parsers/GetProductCategoriesForSKUResponseParser.php
@@ -15,8 +15,8 @@ class GetProductCategoriesForSKUResponseParser extends BaseParser
     public function handle(): Collection
     {
         $responseResult = ($this->shouldWrapInArray())
-                            ? [$this->getResponseResult()['Self']]
-                            : $this->getResponseResult()['Self'];
+                            ? [data_get($this->getResponseResult(), 'Self')]
+                            : data_get($this->getResponseResult(), 'Self');
 
         return collect(['RequestId' => $this->getRequestId(),
                         'Self' => $responseResult,
